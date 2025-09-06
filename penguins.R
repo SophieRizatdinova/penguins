00f277b13546ab0590a2014e3b1c9780a82e096a
Sophie Rizatdinova <sophie.rizatdinova.cdi@gmail.com>
2025-09-06 13:34:00 +0300

    Длина клюва отображена в зависимости от пола

--- penguins.R
+++ penguins.R
@@ -1,8 +1,9 @@
 data <- read.csv("penguins.csv")
 x <- data$bill_length_mm
+g <- data$sex
 
 #Ящик с усами для длины клюва
 
 png("penguins-boxplot.png")
-boxplot(x, ylab = 'Bill length, mm')
+boxplot(x ~ g, xlab = "Sex", ylab = 'Bill length, mm')
 dev.off()
